b92829ade74d01ec251ee8f505ef8250f256e62f
AsadbekFF <79692640+AsadbekFF@users.noreply.github.com>
2021-09-27 17:15:43 +0500

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Вводите текст: ");
+            string text = Console.ReadLine();
+            string result = text[0].ToString();
+
+            if (text is null)
+            {
+                throw new ArgumentNullException(nameof(text), "String is null.");
+            }
+
+            if (string.IsNullOrEmpty(text) || string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException("Incorrect format.");
+            }
+
+            for (int i = 2; i < text.Length; i++)
+            {
+                if (text[i - 1] == '\n')
+                    result += " " + text[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            IsCorrectInput(stringValue);
+            stringValue = stringValue.Trim();
+
+            int result = 0;
+            bool isSigned = stringValue[0] == '-' || stringValue[0] == '+';
+            long thNumber = 1;
+
+            for (int i = stringValue.Length - 1; i >= 0; i--)
+            {
+                if (!isSigned)
+                {
+                    long checking = result + (stringValue[i] - 48L) * thNumber > Int32.MaxValue ? throw new OverflowException()
+                        : result + (stringValue[i] - 48L) * thNumber;
+                    result = (int)checking;
+                    thNumber *= 10;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        if (stringValue[i] == '-')
+                            result *= -1;
+                    }
+                    else
+                    {
+                        long checking = -1 * (result + (stringValue[i] - 48L) * thNumber) < Int32.MinValue
+                            ? throw new OverflowException()
+                            : result + (stringValue[i] - 48L) * thNumber;
+                        result = (int)checking;
+                        thNumber *= 10;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void IsCorrectInput(string stringValue)
+        {
+            if (stringValue is null)
+            {
+                throw new ArgumentNullException(nameof(stringValue), "String is null.");
+            }
+
+            stringValue = stringValue.Trim();
+            if (string.IsNullOrEmpty(stringValue) || string.IsNullOrWhiteSpace(stringValue))
+            {
+                throw new FormatException("String is empty or whiteSpaces.");
+            }
+
+            for (int i = 0; i < stringValue.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if ((stringValue[0] != '-' && stringValue[0] != '+') && !char.IsDigit(stringValue[i]))
+                    {
+                        throw new FormatException("Incorrect Input.");
+                    }
+                }
+                else
+                {
+                    if (!char.IsDigit(stringValue[i]))
+                    {
+                        throw new FormatException("Incorrect input.");
+                    }
+                }
+            }
+        }
+    }
+}
